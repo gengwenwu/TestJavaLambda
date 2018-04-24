6b4f62247fc3c8dc30b236d8ed049c17e9b46d80
“julian” <julian@jollycorp.com>
2018-04-24 15:17:15 +0800

    注释简单修改

--- src/org/logan/lambda/chapter1/Test1.java
+++ src/org/logan/lambda/chapter1/Test1.java
@@ -52,8 +52,7 @@ class Test1 {
 
 	// Lambda 类型推到
 	private static void testTypeInfer() {
-		// 在Lambda表达式中无需指定类型，程序依然可以编译。
-		// 这是因为javac根据程序的上下文在后台推断出了参数的类型。
+		// 在Lambda表达式中无需指定类型，程序依然可以编译。这是因为javac根据程序的上下文在后台推断出了参数的类型。
 		BinaryOperator<Long> add1 = (x, y) -> x + y;
 
 		// 为了便于阅读，可以指定对应的参数类型
@@ -62,7 +61,7 @@ class Test1 {
 		System.out.println("testTypeInfer() -> " + add1.apply(2L, add2.apply(3L, 5L)));
 	}
 
-	// Lambda 引用值,而不是变量
+	// Lambda 引用值，而不是变量
 	private static void testFinalValue() {
 		int z = 3;
 		BinaryOperator<Long> add2 = (x, y) -> {
@@ -75,8 +74,8 @@ class Test1 {
 	}
 
 	// Lambda用在哪里。
-	// 每一个Lambda都能通过一个特定的"函数式接口"与一个给定的类型进行匹配。
-	// 因此一个Lambda表达式能被应用在与其目标类型匹配的任何地方，lambda表达式必须和函数式接口的抽象函数描述一样的参数类型，
+	// 每一个Lambda都能通过一个特定的"函数接口"与一个给定的类型进行匹配。
+	// 因此一个Lambda表达式能被应用在与其目标类型匹配的任何地方，Lambda表达式必须和函数接口的抽象函数描述一样的参数类型，
 	// 它的返回类型也必须和抽象函数的返回类型兼容，并且他能抛出的异常也仅限于在函数的描述范围中。
 	// Java重要的函数接口：Predicate、Consumer、Function、Supplier、UnaryOperator、BinaryOperator。
 
