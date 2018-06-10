f7c62d00425d8533349be35f91ed26f78e35b51c
Logan <490211581@qq.com>
2018-06-11 07:03:07 +0800

    Stream FlatMap

---
+++ src/org/logan/lambda/chapter3/C3_12_StreamApiFlatMap.java
@@ -0,0 +1,29 @@
+package org.logan.lambda.chapter3;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+
+/**
+ * flatMap: TODO <br/>
+ * time: 2018/6/11 上午6:59 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+class C3_12_StreamApiFlatMap {
+
+	public static void main(String[] args) {
+
+		// 例3-12 包含多个列表的 Stream
+		List<Integer> together = Stream.of(asList(1, 2), asList(3, 4))
+				.flatMap(numbers -> numbers.stream())
+				.collect(Collectors.toList());
+
+
+		// TODO
+		// assertEquals(asList(1, 2, 3, 4), together);
+	}
+
+}
