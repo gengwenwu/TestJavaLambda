244d2fd92a77c7a407cd010c76b0396bc4c65886
Logan <490211581@qq.com>
2018-06-12 15:27:01 +0800

    完善重构练习

--- src/org/logan/lambda/chapter3/C3_19_Reconstruction_1.java
+++ src/org/logan/lambda/chapter3/C3_19_Reconstruction_1.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.logan.lambda.common.LogUtil.printEmptyLine;
+
 /**
  * desc: 使用stream api重构练习。<br/>
  * 遗留代码:找出长度大于 1 分钟的曲目。<br/>
@@ -18,11 +20,12 @@ import java.util.stream.Collectors;
 class C3_19_Reconstruction_1 {
 
 	public static void main(String[] args) {
-
+		//TODO
+		printEmptyLine();
 	}
-	
-	// 遗留代码:找出长度大于 1 分钟的曲目
-	public Set<String> findLongTrack1(List<Album> albums) {
+
+
+	private static Set<String> findLongTrack(List<Album> albums) {
 		Set<String> trackNames = new HashSet<>();
 
 		for (Album album : albums) {
@@ -37,23 +40,41 @@ class C3_19_Reconstruction_1 {
 		return trackNames;
 	}
 
-	public Set<String> findLongTracks2(List<Album> albums) {
+//	public static Set<String> test(List<Album> albums) {
+//		//TODO
+//		Set<String> trackNames = new HashSet<>();
+//
+//		for (Album album : albums) {
+//			for (Track track : album.getTrackList()) {
+//				if (track.getLength() > 60) {
+//					String name = track.getName();
+//					trackNames.add(name);
+//				}
+//			}
+//		}
+//
+//		// 遗留代码:找出长度大于 1 分钟的曲目
+//		return trackNames;
+//	}
+
+
+	// 1：将for替换成forEach
+	public static Set<String> findLongTracks1(List<Album> albums) {
 		Set<String> trackNames = new HashSet<>();
-		albums.stream()
-				.forEach(album -> {
-					album.getTracks()
-							.forEach(track -> {
-								if (track.getLength() > 60) {
-									String name = track.getName();
-									trackNames.add(name);
-								}
-							});
-				});
+		albums.stream().forEach(album -> {
+			album.getTracks()
+					.forEach(track -> {
+						if (track.getLength() > 60) {
+							String name = track.getName();
+							trackNames.add(name);
+						}
+					});
+		});
 		return trackNames;
 	}
 
-
-	public Set<String> findLongTracks3(List<Album> albums) {
+	// 2：if 替换成 filter，使用map收集属性
+	public static Set<String> findLongTracks2(List<Album> albums) {
 		Set<String> trackNames = new HashSet<>();
 		albums.stream()
 				.forEach(album -> {
@@ -65,7 +86,8 @@ class C3_19_Reconstruction_1 {
 		return trackNames;
 	}
 
-	public Set<String> findLongTracks4(List<Album> albums) {
+	// 3：flatMap 重构 子List
+	public static Set<String> findLongTracks3(List<Album> albums) {
 		Set<String> trackNames = new HashSet<>();
 		albums.stream()
 				.flatMap(album -> album.getTracks())
@@ -76,7 +98,8 @@ class C3_19_Reconstruction_1 {
 		return trackNames;
 	}
 
-	public Set<String> findLongTracks6(List<Album> albums) {
+	// 4：collect 重构子Set集合
+	public static Set<String> findLongTracks4(List<Album> albums) {
 		return albums.stream()
 				.flatMap(album -> album.getTracks())
 				.filter(track -> track.getLength() > 60)
@@ -84,7 +107,8 @@ class C3_19_Reconstruction_1 {
 				.collect(Collectors.toSet());
 	}
 
-	public Set<String> findLongTracks7(List<Album> albums) {
+	// 5：使用方法属性
+	public static Set<String> findLongTracks5(List<Album> albums) {
 		return albums.stream()
 				.flatMap(Album::getTracks)
 				.filter(track -> track.getLength() > 60)
