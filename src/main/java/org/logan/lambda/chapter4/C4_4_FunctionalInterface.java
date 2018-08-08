180bbbeee9c5655654ea11737fa4765323703b60
Logan <490211581@qq.com>
2018-08-08 11:04:58 +0800

    添加FunctionalInterface注解

---
+++ src/main/java/org/logan/lambda/chapter4/C4_4_FunctionalInterface.java
@@ -0,0 +1,16 @@
+package org.logan.lambda.chapter4;
+
+/**
+ * desc: @FunctionalInterface注解 <br/>
+ * time: 2018/8/8 上午10:54 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+@FunctionalInterface
+interface C4_4_FunctionalInterface { // abstract class
+
+	void method1(); // public abstract
+
+	// void method2();
+
+}
