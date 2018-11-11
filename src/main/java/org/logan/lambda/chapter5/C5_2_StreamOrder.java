2315768e0cda8d2b2adbca5b7fe91bf367805138
Logan <490211581@qq.com>
2018-11-11 14:44:07 +0800

    流的顺序

--- src/main/java/org/logan/lambda/chapter5/C5_2_StreamOrder.java
+++ src/main/java/org/logan/lambda/chapter5/C5_2_StreamOrder.java
@@ -1,11 +1,14 @@
 package org.logan.lambda.chapter5;
 
 /**
- * desc: 流顺序 <br/>
+ * desc: 流中元素的顺序 <br/>
+ * 流是有序的，因为流中的元素都是按顺序处理的。这种顺序称为 出现顺序。出现顺序的定义依赖于数据源和对流的操作。 <br/>
  * time: 2018/11/11 下午1:36 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
  */
-
 class C5_2_StreamOrder {
+
+	// 例子在：org.logan.lambda.chapter5.C5_2_StreamOrderTest
+
 }
---
+++ src/test/java/org/logan/lambda/chapter5/C5_2_StreamOrderTest.java
@@ -0,0 +1,94 @@
+package org.logan.lambda.chapter5;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+/**
+ * desc: 测试流中元素的顺序 <br/>
+ * time: 2018/11/11 下午1:51 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+public class C5_2_StreamOrderTest {
+
+	@Test
+	public void testOrder() {
+		/* 有序集合测试 */
+		for (int i = 0; i < 10000; i++) {
+			List<Integer> numberList = Arrays.asList(1, 2, 3, 4);
+			List<Integer> sameOrderList = numberList.stream()
+					.collect(Collectors.toList());
+			assertEquals(numberList, sameOrderList);
+		}
+	}
+
+	@Test
+	public void testOrder2() {
+		/* 无序集合测试 */
+		for (int i = 0; i < 10000; i++) {
+			Set<Integer> numberSet = new HashSet<>(Arrays.asList(4, 3, 2, 1));
+			List<Integer> numberList = numberSet.stream()
+					.collect(Collectors.toList());
+			assertEquals(Arrays.asList(4, 3, 2, 1), numberList);
+		}
+	}
+
+	@Test
+	public void testOrder3() {
+		/* 无序变有序 */
+		Set<Integer> numberList = new HashSet<>(Arrays.asList(4, 3, 2, 1));
+		List<Integer> sameOrderList = numberList.stream()
+				.sorted() //一些中间操作会产生顺序，如：sorted()
+				.collect(Collectors.toList());
+		assertEquals(Arrays.asList(1, 2, 3, 4), sameOrderList);
+	}
+
+	@Test
+	public void testOrder4() {
+		/*
+			一些中间操作会产生顺序，譬如对值对映射。
+			1，如果进来的流是有序的，那出去的流是也是有序的。
+			2，如果进来的流是无序的，那出去的流是也是无序的。
+		 */
+		List<Integer> numberList = Arrays.asList(1, 2, 3, 4);
+
+		{
+			/* 有序流测试 */
+			for (int i = 0; i < 10000; i++) {
+				List<Integer> stillOrdered = numberList.stream()
+						.map(x -> x + 1)
+						.collect(Collectors.toList());
+
+				// 顺序得到了保留
+				assertEquals(Arrays.asList(2, 3, 4, 5), stillOrdered);
+			}
+		}
+
+		{
+			/* 无序流测试 */
+			for (int i = 0; i < 10000; i++) {
+				Set<Integer> unordered = new HashSet<>(numberList);
+				List<Integer> stillUnordered = unordered.stream()
+						.map(x -> x + 1)
+						.collect(Collectors.toList());
+
+				// 顺序得不到保证
+				assertThat(stillUnordered, hasItem(2));
+				assertThat(stillUnordered, hasItem(3));
+				assertThat(stillUnordered, hasItem(4));
+				assertThat(stillUnordered, hasItem(5));
+			}
+		}
+
+	}
+
+}
