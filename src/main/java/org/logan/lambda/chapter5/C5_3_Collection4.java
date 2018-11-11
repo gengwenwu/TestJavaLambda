937d55c76fd528864a017fbf8163ce076beec5ba
Logan <490211581@qq.com>
2018-11-11 17:16:39 +0800

    多个java文件提交

---
+++ src/main/java/org/logan/lambda/chapter5/C5_3_Collection4.java
@@ -0,0 +1,16 @@
+package org.logan.lambda.chapter5;
+
+/**
+ * desc: TODO <br/>
+ * time: 2018/11/11 下午17:11 <br/>
+ * author: Logan <br/>
+ * since V 1.0 <br/>
+ */
+class C5_3_Collection4 {
+
+	public static void main(String[] args) {
+
+	}
+
+
+}
