2b17d5f11d1a1bed2d758319e2fbdca75dbfb5b8
Logan <490211581@qq.com>
2018-11-11 17:59:49 +0800

    数据分组 groupingBy

--- src/main/java/org/logan/lambda/chapter5/C5_3_Collection4.java
+++ src/main/java/org/logan/lambda/chapter5/C5_3_Collection4.java
@@ -1,7 +1,22 @@
 package org.logan.lambda.chapter5;
 
+import org.logan.lambda.common.SampleData;
+import org.logan.lambda.common.model.Album;
+import org.logan.lambda.common.model.Artist;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
- * desc: TODO <br/>
+ * desc: 数据分组 groupingBy <br/>
+ * {@link Collectors#groupingBy(Function)} <br/>
+ * {@link Collectors#groupingByConcurrent(Function)} <br/>
+ * {@link Collectors#groupingBy(Function, Collector)} <br/>
+ * 等等
  * time: 2018/11/11 下午17:11 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
@@ -9,7 +24,23 @@ package org.logan.lambda.chapter5;
 class C5_3_Collection4 {
 
 	public static void main(String[] args) {
+		albumsByArtist(SampleData.allAlbums.stream());
+	}
 
+	/**
+	 * 使用 Collectors.groupingBy() 对专辑分组
+	 */
+	private static void albumsByArtist(Stream<Album> albums) {
+		/* 主唱对专辑分组 */
+		Map<Artist, List<Album>> albumsByArtist = albums.collect(Collectors.groupingBy(Album::getMainMusician));
+
+		albumsByArtist.forEach((artist, albumList) -> {
+			System.out.println("歌手名称：" + artist.getName());
+
+			albumList.forEach(
+					album -> System.out.println("	专辑名称：" + album.getName())
+			);
+		});
 	}
 
 
