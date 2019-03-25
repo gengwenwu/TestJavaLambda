917d6a8ed37f962806c5a76cb2cdc7f6ca749e9b
Logan <julian@jollycorp.com>
2019-03-25 16:16:56 +0800

    完善

--- src/main/java/org/logan/lambda/chapter6/C6_6_WordCounting.java
+++ src/main/java/org/logan/lambda/chapter6/C6_6_WordCounting.java
@@ -1,11 +1,13 @@
 package org.logan.lambda.chapter6;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.counting;
@@ -19,33 +21,20 @@ import static java.util.stream.Collectors.groupingBy;
  */
 class C6_6_WordCounting {
 
-	public static void main(String[] args) {
-		// TODO: 2019/3/24  
-		InputStream enWiki = C6_6_WordCounting.class.getResourceAsStream("enwiki-20131230-stubs-meta-hist-incr.xml");
-		new C6_6_WordCounting().countUsers(enWiki);
-		// InputStream huckleberryFinn = WordCounting.class.getResourceAsStream("huckleberry_finn");
-		// new WordCounting().countWords(huckleberryFinn);
-	}
+	public static void main(String[] args) throws FileNotFoundException {
+		// 23MB文件
+		String path = System.getProperty("user.dir") + "/resources/log2.txt";
+		System.err.println("====== begin.... ");
 
-	private static final Pattern username = Pattern.compile("\\s+<username>(.*?)</username>");
+		// 1
+		long time = System.currentTimeMillis();
+		new C6_6_WordCounting().countWords(new FileInputStream(new File(path)));
+		System.err.println("===========>use time1:" + (System.currentTimeMillis() - time));
 
-	public void countUsers(InputStream stream) {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
-			Map<String, Long> counts = reader
-					.lines()
-					.parallel()
-					.filter(line -> line.contains("<username>"))
-					.map(line -> {
-						Matcher matcher = username.matcher(line);
-						matcher.find();
-						return matcher.group(1);
-					})
-					.collect(groupingBy(word -> word, counting()));
-
-			counts.forEach((word, count) -> System.out.println(word + " -> " + count));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// 1
+		time = System.currentTimeMillis();
+		new C6_6_WordCounting().countWordsParallel(new FileInputStream(new File(path)));
+		System.err.println("===========>use time2:" + (System.currentTimeMillis() - time));
 	}
 
 	private static final Pattern space = Pattern.compile("\\s+");
@@ -59,7 +48,23 @@ class C6_6_WordCounting {
 					.filter(word -> !word.isEmpty())
 					.collect(groupingBy(word -> word, counting()));
 
-			counts.forEach((word, count) -> System.out.println(word + " -> " + count));
+			//counts.forEach((word, count) -> System.out.println(word + " -> " + count));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void countWordsParallel(InputStream stream) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+			Map<String, Long> counts = reader
+					.lines()
+					.parallel()
+					.flatMap(space::splitAsStream)
+					.map(String::trim)
+					.filter(word -> !word.isEmpty())
+					.collect(groupingBy(word -> word, counting()));
+
+			//counts.forEach((word, count) -> System.out.println(word + " -> " + count));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
