41d11381547fd722c509bd2e2cf9cde6e3be9d59
Logan <julian@jollycorp.com>
2019-03-26 13:46:19 +0800

    优化C6_1_StreamParallel

--- src/main/java/org/logan/lambda/chapter6/C6_1_StreamParallel.java
+++ src/main/java/org/logan/lambda/chapter6/C6_1_StreamParallel.java
@@ -4,26 +4,27 @@ import org.logan.lambda.common.SampleData;
 import org.logan.lambda.common.model.Album;
 import org.logan.lambda.common.model.Track;
 
+import java.util.function.Supplier;
+
 /**
  * desc: 并行化流操作 <br/>
  * time: 2019/3/23 下午8:46 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
  */
-
 class C6_1_StreamParallel {
 
 	public static void main(String[] args) {
-		System.out.println("所有专辑歌曲总长度(串行方式):" + serialArraySum());
-		System.out.println("所有专辑歌曲总长度(并行方式):" + parallelArraySum());
+		recordMethodUsedTime("串行方式", C6_1_StreamParallel::serialArraySum);
+
+		recordMethodUsedTime("并行方式", C6_1_StreamParallel::parallelArraySum);
 	}
 
 	/**
 	 * 所有专辑歌曲总长度 - 串行方式
 	 */
 	private static int serialArraySum() {
-		return SampleData.allAlbums
-				.stream()
+		return SampleData.allAlbums.stream()
 				.flatMap(Album::getTracks)
 				.mapToInt(Track::getLength)
 				.sum();
@@ -33,11 +34,16 @@ class C6_1_StreamParallel {
 	 * 所有专辑歌曲总长度 - 并行方式
 	 */
 	private static int parallelArraySum() {
-		return SampleData.allAlbums
-				.parallelStream()
+		return SampleData.allAlbums.parallelStream()
 				.flatMap(Album::getTracks)
 				.mapToInt(Track::getLength)
 				.sum();
+	}
+
+	public static <T> void recordMethodUsedTime(String flag, Supplier<T> supplier) {
+		long time = System.currentTimeMillis();
+		T t = supplier.get();
+		System.err.println("=======>" + flag + " -> :" + t + ", 运行时长:" + (System.currentTimeMillis() - time));
 	}
 
 }
